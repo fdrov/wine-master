a9f3831455d9015dcb27aa88dce0bbaa3d9dfce2
Fdrv <oldgot@gmail.com>
2021-01-26 19:48:43 +0300

    Раскомментирован код запуска сервера

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 import pandas as pd
@@ -30,5 +31,5 @@ rendered_page = template.render(how_old=how_old,
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
