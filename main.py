9423b90c42e131816eb525cf9f78e47cdb430311
Fdrv <oldgot@gmail.com>
2021-01-27 19:25:49 +0300

    fix изабавление от типа данных в переменных

--- main.py
+++ main.py
@@ -30,18 +30,15 @@ elif last_digit in (2, 3, 4) and (company_age < 12 or company_age > 19):
 else:
     company_age_years = f'{company_age} лет'
 
-# получаем вина по сортам их файла wine2
-wine_data_frame = pd.read_excel(config['PATH_TO_EXCEL'], keep_default_na=False)
-wine_dict = wine_data_frame.to_dict(orient='records')
-
-# форматируем словарь в список словарей для использования в template.html
-wine_list_for_template = collections.defaultdict(list)
-for bottle in wine_dict:
-    wine_list_for_template[bottle['Категория']].append(bottle)
+# получаем вина по сортам их файла каталога
+wines = pd.read_excel(config['PATH_TO_EXCEL'], keep_default_na=False).to_dict(orient='records')
+wine_assortment = collections.defaultdict(list)
+for bottle in wines:
+    wine_assortment[bottle['Категория']].append(bottle)
 
 
 rendered_page = template.render(company_age_years=company_age_years,
-                                wines=wine_list_for_template)
+                                wines=wine_assortment)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
