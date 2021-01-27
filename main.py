44a478e7513c86244d5918ddf5902bb15028d6cf
Fdrv <oldgot@gmail.com>
2021-01-27 14:29:47 +0300

    fix company_age to company_age_years

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ today = datetime.date.today().year
 foundation_year = 1920
 company_age = today - foundation_year
 
+last_digit = company_age % 10
+if last_digit == 1 and company_age != 11:
+    company_age_years = f'{company_age} год'
+elif last_digit in (2, 3, 4) and (company_age < 12 or company_age > 19):
+    company_age_years = f'{company_age} года'
+else:
+    company_age_years = f'{company_age} лет'
+
 # получаем вина по сортам их файла wine2
 wine_data_frame = pd.read_excel(r".\wine3.xlsx", keep_default_na=False)
 wine_dict = wine_data_frame.to_dict(orient='records')
@@ -27,7 +35,7 @@ for bottle in wine_dict:
     wine_list_for_template[bottle['Категория']].append(bottle)
 
 
-rendered_page = template.render(how_old=company_age,
+rendered_page = template.render(company_age_years=company_age_years,
                                 wines=wine_list_for_template)
 
 with open('index.html', 'w', encoding="utf8") as file:
